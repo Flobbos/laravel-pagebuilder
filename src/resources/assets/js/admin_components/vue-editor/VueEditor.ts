4d8dbbf90a6f4e491d11821f2fea2b636f3d0cab
ChristianBielak <christian.bielak@ultrabold.com>
2018-09-26 17:41:01 +0200

    import missing sass assets for quill editor

--- src/resources/assets/js/admin_components/vue-editor/VueEditor.ts
+++ src/resources/assets/js/admin_components/vue-editor/VueEditor.ts
@@ -1,5 +1,7 @@
 import Vue from "vue";
-
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 //@ts-ignore
 import VueQuillEditor from 'vue-quill-editor';
 Vue.use(VueQuillEditor);
