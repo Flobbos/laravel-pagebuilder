cca623cece75c573b554ee501d21cf87b26647e2
ChristianBielak <christian.bielak@ultrabold.com>
2018-11-16 15:52:55 +0100

    fixed bug for saving images

--- src/resources/assets/js/pagebuilder/views/ContentView/ContentView.vue
+++ src/resources/assets/js/pagebuilder/views/ContentView/ContentView.vue
@@ -4,7 +4,7 @@
     <div class="content-view">
         <row-spacer-component v-if="!article.rows || article.rows.length === 0"></row-spacer-component>
 
-
+<!--@Todo Sortable overflow-->
         <div class="row-component-wrapper">
             <draggable v-model="article.rows"  :option="{draggable: '.row-component'}">
                     <row-component @onImageUpload="onImageUpload" v-for="(row, index) in article.rows" :row="row"
