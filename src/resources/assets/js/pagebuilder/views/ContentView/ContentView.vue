9ce610fda6ad1a0d0e3cb662bfb82ffc6a5543e1
ChristianBielak <christian.bielak@ultrabold.com>
2018-11-28 17:59:26 +0100

    draggable styling, and option buttons fix

--- src/resources/assets/js/pagebuilder/views/ContentView/ContentView.vue
+++ src/resources/assets/js/pagebuilder/views/ContentView/ContentView.vue
@@ -2,10 +2,9 @@
     <div class="content-view">
         <row-spacer-component v-if="!article.rows || article.rows.length === 0"></row-spacer-component>
         <div class="row-component-wrapper">
-            <draggable v-model="getArticle.rows" :options="{draggable:'.item'}">
-                <row-component class="item" @onImageUpload="onImageUpload" v-for="(row, index) in getArticle.rows" :row="row"
-                               :key="row.uuid" :array-index="index"></row-component>
-                <row-spacer-component></row-spacer-component>
+            <draggable v-model="getArticle.rows" :options="{filter: '.row-spacer-component'}">
+                    <row-component @onImageUpload="onImageUpload" v-for="(row, index) in getArticle.rows" :row="row"
+                                   :key="row.uuid" :array-index="index"></row-component>
             </draggable>
         </div>
     </div>
