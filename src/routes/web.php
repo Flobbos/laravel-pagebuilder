3b3853754ffde08ed6c6cc05fe9f35eb42356420
Flobbo <Flobbo@192.168.4.3>
2019-02-12 14:43:35 +0100

    Fixed up stubs publishing for Bootstrap 3.

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,16 @@
 
 Route::group(['middleware'=>['web','auth'],'namespace'=>'Flobbos\Pagebuilder\Controllers','prefix'=>'pagebuilder','as'=>'pagebuilder::'], function(){
     Route::get('/','PagebuilderController@index')->name('home');
+    //Resource route for languages
     Route::resource('languages','LanguageController');
+    //Resource route for elements
     Route::resource('element-types', 'ElementTypeController');
+    //Upload photos
     Route::post('upload-photo','UploadController@store')->name('upload');
+    //Delete Photos
     Route::delete('delete-photo','UploadController@destroy')->name('delete-photo');
+    //Delete entire row
     Route::delete('delete-row','PagebuilderController@deleteRow')->name('delete-row');
+    //Delete Column
     Route::delete('delete-column','PagebuilderController@deleteColumn')->name('delete-column');
-    Route::resource('articles','ArticleController');
 });
