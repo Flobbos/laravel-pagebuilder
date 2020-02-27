507158efa94a9ff87eee194fb7d8b6619f7a42d7
Abdelouahab Djoudi <info@djoudi.net>
2020-02-27 02:37:12 +0100

    Add Arabic Translation
    Co-Authored-By: Abdelouahab Djoudi <info@djoudi.net>

---
+++ src/resources/lang/ar/crud.php
@@ -0,0 +1,25 @@
+<?php
+
+//Standard CRUD
+//Arabic - AR
+
+return [
+    //buttons
+    'create_button' => 'محتوى جديد',
+    'save' => 'حفظ',
+    'cancel' => 'إلغاء',
+    'delete' => 'حذف',
+    'edit' => 'تعديل',
+    //Headlines
+    'th_action' => 'العلمية',
+    'create_headline' => 'إضافة محتوى جديد',
+    'edit_headline' => 'تعديل محتوى ',
+    //List
+    'no_entries' => 'لا يوجد محتويات',
+    //Messages
+    'record_created' => 'تم إضافة المحتوى بنجاح',
+    'record_updated' => 'تم تعديل المحتوى بنجاح',
+    'record_deleted' => 'تم حذف المحتوى بنجاح',
+    //File error
+    'invalid_file_upload' => 'حدثت مشكلة أثناء تحميل الملف'
+];
