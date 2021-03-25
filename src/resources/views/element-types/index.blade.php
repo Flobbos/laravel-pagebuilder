17512412c5f73543324d6a69cec6a382ab78f0e5
Alexej Krzewitzki <alexej.k@ultrabold.com>
2021-03-25 13:59:16 +0100

    Fixed table for tailwind

--- src/resources/views/element-types/index.blade.php
+++ src/resources/views/element-types/index.blade.php
@@ -24,31 +24,31 @@
                 @if($element_types->isEmpty())
                 @lang('pagebuilder::crud.no_entries')
                 @else
-                <table class="table table-striped">
+                <table class="w-full mb-4 text-gray-900 shadow-sm">
                     <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
+                        <tr class="bg-gray-200 border-t border-b">
+                            <th class="text-left p-2 font-bold">#</th>
+                            <th class="text-left p-2 font-bold">Name</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($element_types as $element_type)
-                        <tr>
-                            <td>{{$element_type->sorting}}</td>
-                            <td>{{$element_type->name}}</td>
-                            <td>
-                                <div class="btn-group float-right" role="group">
-                                    <a class="btn btn-sm btn-primary" href="{{ route('pagebuilder::element-types.edit',$element_type->id) }}">
-                                        <i class="glyphicon glyphicon-pencil"></i> @lang('pagebuilder::crud.edit')
+                        <tr class="border-b">
+                            <td class="p-2">{{$element_type->sorting}}</td>
+                            <td class="p-2">{{$element_type->name}}</td>
+                            <td class="text-right p-2">
+                                <div class="flex justify-end rounded-lg text-xs mb-1" role="group">
+                                    <a class="bg-blue-500 text-white hover:bg-blue-400 rounded-l-lg px-2 py-1 text-xs mx-0 outline-none focus:shadow-outline" href="{{ route('pagebuilder::element-types.edit',$element_type->id) }}">
+                                        @lang('pagebuilder::crud.edit')
                                     </a>
-                                    <form class="btn-group"
+                                    <form
                                         action="{{ route('pagebuilder::element-types.destroy',$element_type->id) }}"
                                         method="POST">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-                                        <button class="btn btn-sm btn-danger"
-                                                type="submit"><i class="glyphicon glyphicon-trash"></i> @lang('pagebuilder::crud.delete')</button>
+                                        <button class="bg-red-500 text-white hover:bg-red-400 rounded-r-lg px-2 py-1 text-xs mx-0 outline-none focus:shadow-outline"
+                                                type="submit">@lang('pagebuilder::crud.delete')</button>
                                     </form>
                                 </div>
                             </td>
@@ -60,4 +60,4 @@
             </div>
         </div>
     </div>
-@stop
+</x-app-layout>
