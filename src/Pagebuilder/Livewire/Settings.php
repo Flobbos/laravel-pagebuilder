6d7e4bf34f326a3862b1adcb3a7badcfd9a67bbe
Alexej Krzewitzki <alexej.k@ultrabold.com>
2021-03-25 16:39:51 +0100

    Updated changelog Added first livewire component

---
+++ src/Pagebuilder/Livewire/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Flobbos\Pagebuilder\Livewire;
+
+use Livewire\Component;
+
+class Settings extends Component
+{
+    public function render()
+    {
+        return view('pagebuilder::livewire.settings');
+    }
+}
---
+++ src/resources/views/livewire/settings.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- put component here -->
+    Hello!
+</div>
