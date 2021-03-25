19cdd668684d7a63ec9a3e39fb42b06ec20faff7
Alexej Krzewitzki <alexej.k@ultrabold.com>
2021-03-25 13:42:55 +0100

    First updates for Tailwind

--- src/resources/views/element-types/edit.blade.php
+++ src/resources/views/element-types/edit.blade.php
@@ -1,63 +1,47 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
-
-                <form action="{{ route('pagebuilder::element-types.update',$element_type->id) }}" role="form" method="POST"  enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    {{method_field('PUT')}}
-
-                    <div class="card-header">
-                        <h3>Elemente</h3>
-                        @lang('pagebuilder::crud.create_headline')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Element') }}
+        </h2>
+    </x-slot>
+    <div class="container xl mx-auto mt-5 py-5">
+        <form action="{{ route('pagebuilder::element-types.update',$element_type->id) }}" role="form" method="POST"  enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="flex flex-col relative bg-white rounded border border-gray-300">
+            <div class="flex-auto p-5">
+                <h3 class="mb-3 text-xl">Elemente</h3>
+                @lang('pagebuilder::crud.create_headline')
+            </div>
+            <div class="flex-auto p-5">
+                @include('pagebuilder::notifications')
+                <div class="mb-4">
+                    <label class="inline-block mb-2" for="name">Name</label>
+                    <input class="form-control" type="text" name="name" value="{{old('name')}}" />
+                </div>
+                <div class="mb-4">
+                    <label class="inline-block mb-2" for="name">Component</label>
+                    <input class="form-control" type="text" name="component" value="{{old('component')}}" />
+                </div>
+                <div class="mb-4">
+                    <label class="inline-block mb-2" for="name">Icon</label>
+                    <input class="form-control" type="text" name="icon" value="{{old('icon')}}" />
+                </div>
+                <div class="mb-4">
+                    <label class="inline-block mb-2" for="name">Sortierung</label>
+                    <input class="form-control" type="text" name="sorting" value="{{old('sorting')}}" />
+                </div>
+            </div>
+            <div class="bg-gray-100 rounded-b p-5">
+                <div class="grid grid-cols-2">
+                    <div>
+                        <a href="{{ route('pagebuilder::element-types.index') }}" class="bg-red-500 hover:bg-red-400 text-white hover:text-red-100 text-normal px-3 py2 rounded">{{ trans('pagebuilder::crud.cancel') }}</a>
                     </div>
-
-                    <div class="card-body">
-                        
-                        @include('pagebuilder::notifications')
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input class="form-control" type="text" name="name" value="{{old('name',$element_type->name)}}" />
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Component</label>
-                            <input class="form-control" type="text" name="component" value="{{old('component',$element_type->component)}}" />
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Icon</label>
-                            <input class="form-control" type="text" name="icon" value="{{old('icon',$element_type->icon)}}" />
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Sortierung</label>
-                            <input class="form-control" type="text" name="sorting" value="{{old('sorting',$element_type->sorting)}}" />
-                        </div>
-                            
+                    <div class="text-right">
+                        <button type="submit" class="bg-green-500 hover:bg-green-400 text-white hover:text-green-100 text-normal px-3 py2 rounded">{{ trans('pagebuilder::crud.save') }}</button>
                     </div>
-
-                    <div class="card-footer">
-
-                        <div class="row">
-
-                            <div class="col-sm-6">
-                                <a href="{{ route('pagebuilder::element-types.index') }}" class="btn btn-danger">{{ trans('pagebuilder::crud.cancel') }}</a>
-                            </div>
-
-                            <div class="col-sm-6 text-right">
-                                <button type="submit" class="btn btn-success">{{ trans('pagebuilder::crud.save') }}</button>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </form>
-
+                </div>
             </div>
         </div>
-
+        </form>
     </div>
-</div>
-@stop
+</x-app-layout>
--- src/resources/views/element-types/index.blade.php
+++ src/resources/views/element-types/index.blade.php
@@ -1,63 +1,63 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h3>Elemente</h3>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="{{ route('pagebuilder::element-types.create') }}" class="btn btn-primary btn-small float-right">
-                                <i class="glyphicon glyphicon-plus"></i> @lang('pagebuilder::crud.create_button')
-                            </a>
-                        </div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Element') }}
+        </h2>
+    </x-slot>
+    <div class="container xl mx-auto mt-5 py-5">
+        <div class="flex flex-col relative bg-white rounded border border-gray-300">
+            <div class="flex-auto p-5">
+                <div class="grid grid-cols-2">
+                    <div>
+                        <h3>Elemente</h3>
+                    </div>
+                    <div>
+                        <a href="{{ route('pagebuilder::element-types.create') }}" class="bg-blue-500 hover:bg-blue-400 text-white hover:text-blue-100 text-normal px-3 py2 rounded">
+                            @lang('pagebuilder::crud.create_button')
+                        </a>
                     </div>
                 </div>
+            </div>
 
-                <div class="card-body">
-                    @include('pagebuilder::notifications')
-                    @if($element_types->isEmpty())
-                    @lang('pagebuilder::crud.no_entries')
-                    @else
-                    <table class="table table-striped">
-                        <thead>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th></th>
-                        </thead>
-                        <tbody>
-                            @foreach($element_types as $element_type)
-                            <tr>
-                                <td>{{$element_type->sorting}}</td>
-                                <td>{{$element_type->name}}</td>
-                                <td>
-                                    <div class="btn-group float-right" role="group">
-                                        <a class="btn btn-sm btn-primary" href="{{ route('pagebuilder::element-types.edit',$element_type->id) }}">
-                                            <i class="glyphicon glyphicon-pencil"></i> @lang('pagebuilder::crud.edit')
-                                        </a>
-                                        <form class="btn-group"
-                                            action="{{ route('pagebuilder::element-types.destroy',$element_type->id) }}"
-                                            method="POST" v-on:submit.prevent="confirmDelete">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <button class="btn btn-sm btn-danger"
-                                                    type="submit"><i class="glyphicon glyphicon-trash"></i> @lang('pagebuilder::crud.delete')</button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @endif
-                </div>
+            <div class="flex-auto p-5">
+                @include('pagebuilder::notifications')
+                @if($element_types->isEmpty())
+                @lang('pagebuilder::crud.no_entries')
+                @else
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($element_types as $element_type)
+                        <tr>
+                            <td>{{$element_type->sorting}}</td>
+                            <td>{{$element_type->name}}</td>
+                            <td>
+                                <div class="btn-group float-right" role="group">
+                                    <a class="btn btn-sm btn-primary" href="{{ route('pagebuilder::element-types.edit',$element_type->id) }}">
+                                        <i class="glyphicon glyphicon-pencil"></i> @lang('pagebuilder::crud.edit')
+                                    </a>
+                                    <form class="btn-group"
+                                        action="{{ route('pagebuilder::element-types.destroy',$element_type->id) }}"
+                                        method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button class="btn btn-sm btn-danger"
+                                                type="submit"><i class="glyphicon glyphicon-trash"></i> @lang('pagebuilder::crud.delete')</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @endif
             </div>
         </div>
     </div>
-</div>
-<simplert ref="simplert"></simplert>
 @stop
